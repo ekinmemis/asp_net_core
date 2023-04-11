824e3feba8c52fdbba9ae418a40712e74cd82346
Ekin Memiş <memis.ekin@gmail.com>
2023-04-11 15:59:36 +0300

    middleware added

--- WebStartup/Program.cs
+++ WebStartup/Program.cs
@@ -1,3 +1,5 @@
+using WebStartup.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,9 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IStartupFilter, RequestSetOptionsStartupFilter>();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
