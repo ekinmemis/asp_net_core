e75fc274b5f325a6c0da2c2bb8c6ff512dffd0ec
Ekin Memiş <memis.ekin@gmail.com>
2023-04-11 15:31:08 +0300

    Minimal api with todo app

---
+++ TodoApi2/Models/Todo.cs
@@ -0,0 +1,10 @@
+namespace TodoApi2.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
+
