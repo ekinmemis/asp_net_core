fd46b0a346d4b2997d5eeefe6aa5711ffc4ec1ea
Ekin Memiş <memis.ekin@gmail.com>
2023-04-11 16:01:44 +0300

    DependencyInjectionMVC project created

---
+++ DependencyInjectionMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DependencyInjectionMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
