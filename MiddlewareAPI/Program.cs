095cf3af6dd3842deb84afd0da9f3ab4ac4c10c0
Ekin Memiş <memis.ekin@gmail.com>
2023-04-12 15:41:29 +0300

    middleware added in program.cs

---
+++ MiddlewareAPI/Program.cs
@@ -0,0 +1,55 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+app.Use(async (context, next) =>
+{
+    await next.Invoke();
+});
+
+app.Run(async context =>
+{
+    await context.Response.WriteAsync("Hello world!");
+});
+
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseDatabaseErrorPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseCookiePolicy();
+
+app.UseRouting();
+app.UseRequestLocalization();
+app.UseCors();
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseSession();
+app.UseResponseCompression();
+app.UseResponseCaching();
+
+app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
