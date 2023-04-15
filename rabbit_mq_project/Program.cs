adbb20b3b85d9152ac4e2be9a8df638f8d9c8419
Ekin Memiş <memis.ekin@gmail.com>
2023-04-15 15:47:30 +0300

    rabbitmq project created

--- rabbit_mq_project/Program.cs
+++ rabbit_mq_project/Program.cs
@@ -1,6 +1,22 @@
-﻿using RabbitMQ.Client;
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
-ConnectionFactory factory = new ConnectionFactory();
-factory.Uri = new Uri("http://localhost:15672/");
-factory.HostName = "localhost";
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.Uri = new Uri("http://localhost:15672/");
+        factory.HostName = "localhost";
 
+        using (IConnection connection = factory.CreateConnection())
+        using (IModel channel = connection.CreateModel())
+        {
+            channel.QueueDeclare("mesajkuyrugu", false, false, true);
+            byte[] bytemessage = Encoding.UTF8.GetBytes("sevdim seni 1 kere");
+            channel.BasicPublish(exchange: "", routingKey: "mesajkuyrugu", body: bytemessage);
+        }
+        Console.Read();
+    }
+}
